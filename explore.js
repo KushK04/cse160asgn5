d2c8e294d07c88156dfc0856f9e0a4e49d35145f
Kush <kkush4423@gmail.com>
2025-03-14 21:34:07 -0700

    Finish

---
+++ explore.js
@@ -0,0 +1,8 @@
+function main() {
+    // Retrieve <canvas> element <- (1)
+    var canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+ }  
